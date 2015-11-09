5e586e22d34c5a70135a2a3f4883b349b990faaf
YAT <yat@yat-net.com>
2015-11-09 11:25:41 +0900

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,95 @@
+/*
+ * Global variables
+ */
+var gulp = require( 'gulp' ),
+	scss = require( 'gulp-sass' ),
+	browserSync = require( 'browser-sync' ),
+	plumber = require( 'gulp-plumber' ),//エラー通知
+	notify = require( 'gulp-notify' ),//エラー通知
+	imagemin = require( 'gulp-imagemin' ),//画像圧縮
+	imageminPngquant = require( 'imagemin-pngquant' ),//png画像の圧縮
+	pleeease = require( 'gulp-pleeease' ),//ベンダープレフィックス
+	sourcemaps = require('gulp-sourcemaps'),
+	paths = {
+		rootDir : './',
+		srcDir : 'dev.images/',
+		dstDir : 'img/'
+	}
+
+/*
+ * Sass
+ */
+gulp.task( 'scss', function() {
+	gulp.src( paths.rootDir + 'scss/**/*.scss' )
+		.pipe( sourcemaps.init() )
+		.pipe( scss() )
+		.pipe( pleeease() )
+		.pipe( plumber({
+			errorHandler: notify.onError( 'Error: <%= error.message %>' )
+		}) )
+		.pipe( sourcemaps.write( './' ) )
+		.pipe( gulp.dest( paths.rootDir + 'css' ) );
+});
+
+/*
+ * Pleeease
+ */
+gulp.task('pleeease', function() {
+	return gulp.src( paths.rootDir + 'css/*.css' )
+		.pipe( pleeease({
+			// minifier: false, //圧縮の有無 true/false
+			sass: true
+		}) )
+		.pipe( plumber ( {
+			errorHandler: notify.onError( 'Error: <%= error.message %>' )
+		} ) )
+		.pipe( gulp.dest( paths.rootDir + 'css' ) );
+});
+
+/*
+ * Imagemin
+ */
+gulp.task( 'imagemin', function(){
+	var srcGlob = paths.srcDir + '**/*.+(jpg|jpeg|gif|svg)';
+	var dstGlob = paths.dstDir;
+	var imageminOptions = {
+		optimizationLevel: 7
+	};
+
+	gulp.src( srcGlob )
+		.pipe( imagemin( imageminOptions ) )
+		.pipe( gulp.dest( 'img' ) );
+});
+
+gulp.task('imageminPngquant', function () {
+	gulp.src( paths.srcDir + '**/*.png' )
+		.pipe( imageminPngquant( {quality: '65-80', speed: 1 } )())
+		.pipe( gulp.dest( 'img' ) );
+});
+
+/*
+ * Browser-sync
+ */
+gulp.task( 'browser-sync', function() {
+	browserSync.init({
+		proxy: "vccw.dev",
+		notify: true
+	});
+});
+
+gulp.task( 'bs-reload', function () {
+	browserSync.reload();
+});
+
+/*
+ * Default
+ */
+gulp.task( 'default', ['browser-sync'], function() {
+	var bsList = [
+		paths.rootDir + 'js/**/*.js',
+		paths.rootDir + 'css/*.css',
+		paths.rootDir + '**/*.php'
+	];
+	gulp.watch( paths.rootDir + 'scss/**/*.scss', ['scss'] );
+	gulp.watch( bsList, ['bs-reload']);
+});
---
+++ readme.md
@@ -0,0 +1,48 @@
+# Gulp+WordPress 開発環境構築
+
+## パッケージ一覧
+* gulp
+* gulp-sass
+* browserSync
+* plumber //エラー通知
+* notify //エラー通知
+* gulp-imagemin //画像圧縮
+* imagemin-pngquant //png画像の圧縮
+* gulp-pleeease //ベンダープレフィックス
+* gulp-sourcemaps //ソースマップ
+
+## 依存アプリケーション等
+
+* [npm](https://www.npmjs.com/)
+* [gulp](http://gulpjs.com/)
+* [sass](http://sass-lang.com/)
+
+## 設定ファイル
+
+* .editorcoding 文字コード設定
+
+
+### インストール
+
+#### npmインストール
+` npm install `
+
+エラーが出たら `sudo` をつける
+
+
+### Usage
+
+### 起動コマンド
+` gulp `　- defaultで設定しているtaskが起動する
+
+` gulp *** ` - *** のtaskを起動する
+
+### Author
+
+YAT [http://wp.yat-net.com](http://wp.yat-net.com)
+
+Chiaki [http://mellowchanter.info/](http://mellowchanter.info/)
+
+###Version
+
+2015.11.09 0.1 beta test
